b7c8ce324201985ea6004ea8e52aefab489402cf
shunyavkash-krina-paghadal <krina.paghadal@shunyavkash.com>
2025-04-15 18:33:09 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,4 +65,40 @@ function updateCountdown() {
 }
 setInterval(updateCountdown, 1000);
 
-// Loader
+// Fancybox
+Fancybox.bind("[data-fancybox]", {
+  // options if needed
+  YouTube: {
+    autoplay: true,
+  },
+});
+
+// Conuter-1
+
+const counters = document.querySelectorAll(".counter");
+const speed = 200; // The lower the slower
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    // Lower inc to slow and higher to slow
+    const inc = target / speed;
+
+    // console.log(inc);
+    // console.log(count);
+
+    // Check if target is reached
+    if (count < target) {
+      // Add inc to count and output in counter
+      counter.innerText = count + inc;
+      // Call function every ms
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
